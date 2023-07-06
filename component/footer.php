55eed38badaaa943f87b997a4d240c45fd41dbcf
Greatness Abolade <49532326+thegrtnx@users.noreply.github.com>
2023-07-06 07:30:12 +0100

    Update footer.php

--- component/footer.php
+++ component/footer.php
@@ -2,7 +2,7 @@
 <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
   <small>
   <div class="mb-2 mb-md-0">
-     Developed by <a href="https://www.google.com/search?client=opera&q=abolade+greatness&sourceid=opera&ie=UTF-8&oe=UTF-8" target="_blank" class="footer-link fw-bolder"> Ajayi Favour </a>
+     Developed by Ajayi Favour 
   </div>
   </small>
   <p id="demo" hidden></p>
