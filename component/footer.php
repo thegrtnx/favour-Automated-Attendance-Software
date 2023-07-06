c18432a1481828f79b189ca6399f216e0a051b70
Greatness Abolade <49532326+thegrtnx@users.noreply.github.com>
2023-07-06 07:28:27 +0100

    Update footer.php

--- component/footer.php
+++ component/footer.php
@@ -2,8 +2,7 @@
 <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
   <small>
   <div class="mb-2 mb-md-0">
-     Developed by <a href="https://www.google.com/search?client=opera&q=abolade+greatness&sourceid=opera&ie=UTF-8&oe=UTF-8" target="_blank" class="footer-link fw-bolder">Abolade Greatness</a> | Powered by
-    <a href="https://hynitr.com" target="_blank" class="footer-link fw-bolder">Hynitr</a>
+     Developed by <a href="https://www.google.com/search?client=opera&q=abolade+greatness&sourceid=opera&ie=UTF-8&oe=UTF-8" target="_blank" class="footer-link fw-bolder"> Favour Ajayi </a>
   </div>
   </small>
   <p id="demo" hidden></p>
